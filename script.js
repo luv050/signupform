0733a0df234efccd31114103e5aaa326584267f0
luv050 <luvrs050@gmail.com>
2024-07-06 17:17:17 +0530

    doing

--- script.js
+++ script.js
@@ -30,3 +30,51 @@ pass.addEventListener("keyup", (element) => {
         passError.style.borderColor = "red";
     }
 });
+
+confirm.addEventListener("keyup", (element) => {
+    if (!checkSpacesInString(confirm.value)) {
+        if (!checkIfEmptyString(confirm.value)) {
+            if (checkMinMaxChars(confirm.value)) {
+                if (checkIfEqual(pass.value, confirm.value)) {
+                    confirmError.style.borderColor = "green";
+                    passError.style.borderColor = "green";
+                    passError.textContent = "";
+                    confirmError.textContent = "";
+                } else {
+                    confirmError.textContent = "Passwords do not match";
+                    confirmError.style.borderColor = "red";
+                }
+            } else {
+                confirmError.style.borderColor = "red";
+                confirmError.textContent = "Must contain 6-20 characters.";
+            }
+
+        } else {
+            confirmError.textContent = "You must enter a password";
+            confirmError.style.borderColor = "red";
+        }
+    } else {
+        confirmError.textContent = "Password cannot contain spaces";
+        confirmError.style.borderColor = "red";
+    }
+});
+
+function checkSpacesInString(str) {
+    return str.includes(" ");
+}
+
+function checkIfEqual(val1, val2) {
+    return (val1 == val2);
+}
+
+function checkIfEmptyString(str) {
+    return (str === null || str === "");
+}
+
+function checkMinMaxChars(str) {
+    return (str.length > 5 && str.length < 21);
+}
+
+//password must be:
+// 6 chars at least, no more than 20 chars
+// no spaces
